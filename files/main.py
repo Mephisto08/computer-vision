ecd24c272eebd800c1f2d7b83028f1d70de25f9a
stnswern <NStudKMW08#>
2022-05-03 10:58:46 +0200

    asdf

--- files/main.py
+++ files/main.py
@@ -1,38 +1,53 @@
-import imutils
-import cv2
+import sys
+import cv2 as cv
+import numpy as np
 
-def showAndWait(name, img):
-    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(name, (500, 700))
-    cv2.imshow(name, img)
-    cv2.waitKey(0) 
+def showAndWait(img):
+    cv.namedWindow("Image", cv.WINDOW_NORMAL)
+    cv.resizeWindow("Image", (500, 700))
+    cv.imshow("Image", img)
+    cv.waitKey(0)
 
-image = cv2.imread("messerMuenze.jpg")
-showAndWait("Image", image)
+def main(argv):
+    
+    default_file = 'muenzen.jpg'
+    filename = argv[0] if len(argv) > 0 else default_file
+    # Loads an image
+    src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image!')
+        print ('Usage: hough_circle.py [image_name -- default ' + default_file + '] \n')
+        return -1
+    
+    
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    
+    
+    gray = cv.medianBlur(gray, 5)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-showAndWait("Image", gray)
+    showAndWait(gray)
+    
+    rows = gray.shape[0]
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=30,
+                               minRadius=1, maxRadius=30)
+    
+    
+    print(circles)
 
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-showAndWait("Image", gray)
-
-edged = cv2.Canny(gray, 50, 100)
-showAndWait("Image", edged)
-edged = cv2.dilate(edged, None, iterations=1)
-showAndWait("dilate 1", edged)
-edged = cv2.dilate(edged, None, iterations=1)
-showAndWait("dilate 2", edged)
-
-edged = cv2.dilate(edged, None, iterations=1)
-showAndWait("dilate 3", edged)
-edged = cv2.erode(edged, None, iterations=1)
-showAndWait("erode 2", edged)
-
-# find contours in the edge map
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-    cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grabcontours(cnts)
-# sort the contours from left-to-right and initialize the
-# 'pixels per metric' calibration variable
-(cnts, ) = imutils.contours.sort_contours(cnts)
-pixelsPerMetric = None
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv.circle(src, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv.circle(src, center, radius, (255, 0, 255), 3)
+    
+    showAndWait(src)
+    
+    return 0
+if __name__ == "__main__":
+    main(sys.argv[1:])
